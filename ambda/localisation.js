d4bb14aed5ed790ccd144561ac0ae3e07eb7e0dc
cylia06 <cylia.silouh@gmail.com>
2021-01-11 00:42:11 +0100

    architecture et debut du projet

---
+++ ambda/localisation.js
@@ -0,0 +1,26 @@
+module.exports = {
+    en: {
+        translation: {
+            WELCOME_MSG: 'Welcome to my deadlines ',
+            REGISTER_MSG: 'Your deadline {{name}} is {{month}} {{day}} {{year}}.',
+            REJECTED_MSG: 'OK. Give me another deadline date',
+            HELP_MSG: 'You can say Hello to me ! How can I help ?',
+            REFLECTOR_MSG: 'You just triggered {{intent}}',
+            GOODBYE_MSG: 'Goodbye !',
+            FALLBACK_MSG: 'Sorry, I don\t know about that. Please try again',
+            ERROR_MSG: 'Sorry, there was an error. Please try again'
+        }
+    },
+    fr: {
+        translation: {
+            WELCOME_MSG: 'Bienvenue dans la skil des échéances ',
+            REGISTER_MSG: 'ton échéance de {{name}} est le {{month}} {{day}} {{year}}.',
+            REJECTED_MSG: "D'accord. Je ne vais pas prendre en compte cette date. Donnez-moi une autre date pour que je puisse l'enregistrer",
+            HELP_MSG: "Je peux me souvenir de votre date d'échéance de {{name}} . Dites-moi le jour, mois et année de naissance ou bien dites moi simplement \"enregistre mon échéance\". ",
+            REFLECTOR_MSG: "Vous avez invoqué l'intention {{intent}}",
+            GOODBYE_MSG: ['Au revoir!', 'A bientôt', 'A la prochaine fois'],
+            FALLBACK_MSG: 'Désolé, je ne sais pas. Pouvez vous reformuler ?',
+            ERROR_MSG: "Désolé, je n'ai pas compris. Pouvez vous reformuler ?"
+        }
+    }
+}
